6785e0524236397098337552ccbf042482858bfc
Kornelijus Sliubauskas <kornelijus.sliubauskas@gmail.com>
2022-09-21 11:37:07 +0200

    feat: appointment services "getAllAppointments", "createAppointment"

---
+++ backend/src/components/appointment/appointment.service.js
@@ -0,0 +1,53 @@
+// DEBUG
+import debug from "debug";
+
+// MODELS
+import Appointment from "../../database/models/Appointment";
+
+// DAL
+import appointmentDAO from "./appointment.DAO";
+
+// UTILS
+
+import appointmentUtils from "./appointment.utils";
+
+// ---------------------------------------------------------------------------------
+
+const errorLogger = debug("addendum:appointment.service:error");
+
+class AppointmentService {
+  async getAllAppointments() {
+    try {
+      return appointmentDAO.getAppointments();
+    } catch (error) {
+      errorLogger(error);
+      throw error;
+    }
+  }
+
+  async createAppointment(appointmentDTO) {
+    try {
+      const { patientName, patientSurname, appointmentDate } = appointmentDTO;
+
+      const patientsLastAppointment =
+        await appointmentDAO.findPatientsLastAppointmentByPatientNameAndPatientSurname(
+          patientName,
+          patientSurname,
+        );
+
+      if (patientsLastAppointment) {
+        appointmentUtils.checkIfAppointmentIsNotEarlierThanAWeek({
+          patientsLastAppointmentDate: patientsLastAppointment.appointmentDate,
+          appointmentDate,
+        });
+      }
+
+      await appointmentDAO.createAppointment(appointmentDTO);
+    } catch (error) {
+      errorLogger(error);
+      throw error;
+    }
+  }
+}
+
+export default new AppointmentService();
