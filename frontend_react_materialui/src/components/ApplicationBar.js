ca5cb2ade61b6e2361978d17729640398832509c
Kornelijus Sliubauskas <kornelijus.sliubauskas@gmail.com>
2022-09-22 16:46:37 +0200

    feat: created AppointmentBar component

---
+++ frontend_react_materialui/src/components/ApplicationBar.js
@@ -0,0 +1,70 @@
+// REACT
+import * as React from "react";
+
+// REACT MUI
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Typography from "@mui/material/Typography";
+import MuiAppBar from "@mui/material/AppBar";
+import { styled } from "@mui/material/styles";
+
+// COMPONENTS
+import FlagsSelect from "./FlagsSelect";
+
+// ---------------------------------------------------------------------------
+
+const drawerWidth = 240;
+
+const StyledApplicationBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+export default function ApplicationBar({ open, toggleDrawer }) {
+  return (
+    <StyledApplicationBar position="absolute" open={open}>
+      <Toolbar
+        sx={{
+          pr: "24px",
+        }}
+      >
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="open drawer"
+          onClick={toggleDrawer}
+          sx={{
+            marginRight: "36px",
+            ...(open && { display: "none" }),
+          }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography
+          component="h1"
+          variant="h6"
+          color="inherit"
+          noWrap
+          sx={{ flexGrow: 1 }}
+        >
+          Addendum
+        </Typography>
+        <FlagsSelect />
+      </Toolbar>
+    </StyledApplicationBar>
+  );
+}
