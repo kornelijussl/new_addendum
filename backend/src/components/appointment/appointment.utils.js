d68781da247e52c4a17bfd19051f6754d619308b
Kornelijus Sliubauskas <kornelijus.sliubauskas@gmail.com>
2022-09-22 16:42:15 +0200

    chore: semantic changes to passed arguments

--- backend/src/components/appointment/appointment.utils.js
+++ backend/src/components/appointment/appointment.utils.js
@@ -1,16 +1,19 @@
 class AppointmentUtils {
   checkIfAppointmentIsNotEarlierThanAWeek({
     patientsLastAppointmentDate,
-    appointmentDate,
+    appointmentDateTime,
   }) {
     const patientsLastAppointmentDateInMilliseconds = Date.parse(
       patientsLastAppointmentDate,
     );
-    const appointmentDateInMilliseconds = Date.parse(appointmentDate);
+    const appointmentDateInMilliseconds = Date.parse(appointmentDateTime);
 
     const weekInMilliseconds = 1.65344e9;
 
-    if (patientsLastAppointmentDateInMilliseconds + weekInMilliseconds > appointmentDateInMilliseconds) {
+    if (
+      patientsLastAppointmentDateInMilliseconds + weekInMilliseconds >
+      appointmentDateInMilliseconds
+    ) {
       throw "Appointment is earlier than a week";
     }
   }
