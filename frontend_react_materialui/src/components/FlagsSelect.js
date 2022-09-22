7823cacfef5836fe66a1098a1938e36f924182ec
Kornelijus Sliubauskas <kornelijus.sliubauskas@gmail.com>
2022-09-22 17:27:59 +0200

    feat: created flags select component

---
+++ frontend_react_materialui/src/components/FlagsSelect.js
@@ -0,0 +1,37 @@
+// REACT
+import * as React from "react";
+
+// REACT FLAGS SELECT
+import ReactFlagsSelect from "react-flags-select";
+
+// REACT-I18NEXT
+import { useTranslation } from "react-i18next";
+
+// CONTEXT
+import { i18nContext } from "../context/I18nContext";
+
+// CSS
+import "./FlagsSelect.css";
+
+// ------------------------------------------------------------
+
+export default function FlagsSelect() {
+  // ### CONTEXT
+  const { selectedCountryFlag, setSelectedCountryFlag } =
+    React.useContext(i18nContext);
+
+  // ### HOOKS
+  const { i18n } = useTranslation();
+
+  return (
+    <ReactFlagsSelect
+      fullWidth={false}
+      countries={["GB", "LT"]}
+      selected={selectedCountryFlag}
+      onSelect={(code) => {
+        i18n.changeLanguage(code);
+        setSelectedCountryFlag(code);
+      }}
+    />
+  );
+}
