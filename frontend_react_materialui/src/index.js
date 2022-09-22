a8c444fcc2e6ad67aa6398a8b412b48d058083bc
Kornelijus Sliubauskas <kornelijus.sliubauskas@gmail.com>
2022-09-22 16:45:38 +0200

    feat: react entry point

---
+++ frontend_react_materialui/src/index.js
@@ -0,0 +1,33 @@
+// REACT
+import * as React from "react";
+
+// REACT DOM
+import ReactDOM from "react-dom/client";
+
+// NOTISTACK
+import { SnackbarProvider } from "notistack";
+
+// REACT BROWSER DOM
+import { BrowserRouter as Router } from "react-router-dom";
+
+// ROUTES
+import Routes from "./router/Routes";
+
+// i18n
+import "./i18n";
+import I18nContextProvider from "./context/I18nContext";
+
+// ----------------------------------------------------------------------------------------------------
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <SnackbarProvider>
+        <I18nContextProvider>
+          <Routes />
+        </I18nContextProvider>
+      </SnackbarProvider>
+    </Router>
+  </React.StrictMode>,
+);
